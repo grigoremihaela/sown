73a98f0fdede5f618f26773ea9b008539ab0c3a1
Mihaela <mihaelaagrigore@gmail.com>
2018-03-22 11:48:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {currentCount: 0}
+  }
+  timer() {
+    this.setState({
+      currentCount: this.state.currentCount + 1
+    })
+    if(this.state.currentCount < 0) { 
+      clearInterval(this.intervalId);
+    }
+  }
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer.bind(this), 1000);
+  }
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>{this.state.currentCount}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
