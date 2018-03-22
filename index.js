98d46bf4a104d66e569e0a4afed04d36f4bd42f1
Mihaela <mihaelaagrigore@gmail.com>
2018-03-22 13:36:59 +0200

    add rpi-gpio

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express'); 
+var app = express();
+var path = require('path');
+var gpio = require('rpi-gpio');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+console.log(path.join(__dirname, 'public'));
+
+gpio.setup(11, gpio.DIR_OUT);
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+app.get('/', function(req, res){ 
+  //res.render('index',{status:"Press Button To change Status of Led !!"});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+gpio.on('change', function(channel, value) {
+  console.log('Channel ' + channel + ' value is now ' + value);
+  gpio.write(11, value, function(err) {
+    if (err) throw err;
+    //console.log('Written ' + value + ' to pin 11');
+  });
+});
+/*
+app.post('/led/on', function(req, res){
+  gpio.write(11, true, function(err) {
+        if (err) throw err;
+        console.log('Written True to pin');
+	console.log(path.join(__dirname, 'public'));
+	return res.render('index', {status: "Cool!!Led is On"});
+    });
+});
+
+app.post('/led/off', function(req, res){
+  gpio.write(11, false, function(err) {
+        if (err) throw err;
+        console.log('Written False to pin');
+	console.log(path.join(__dirname, 'public'));
+	return res.render('index',{status: "Ohh!! Led is Off"});
+    });
+});
+*/
+app.listen(3000, function () {
+  console.log('Simple LED Control Server Started on Port: 3000!')
+});
