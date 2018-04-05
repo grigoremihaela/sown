cb500b853dfdb218cdfb86692049bf5a8dbea319
grigoremihaela <grigoremihaela@ymail.com>
2018-04-05 12:35:53 +0300

    test pigpio

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var app = express();
 
 'use strict';
 
-var Gpio = require('../').Gpio,
+var Gpio = require('Gpio'),
   gpio,
   gpioNo;
 
@@ -16,3 +16,28 @@ for (gpioNo = Gpio.MIN_GPIO; gpioNo <= Gpio.MAX_GPIO; gpioNo += 1) {
     ' level=' + gpio.digitalRead()
   );
 }
+
+/*
+var gpio = require('rpi-gpio');
+var buttonState;
+
+gpio.setup(16, gpio.DIR_OUT);
+
+app.on('stateChange', function(previousValue, value){
+  console.log('button state changed from', previousValue, 'to', value);
+});
+
+setInterval(function(){
+  gpio.read(16, function(err, value) {
+    if(err){
+      app.emit('error', err);
+    } else{
+      if(buttonState !== value){
+        var previousState = buttonState;
+        buttonState = value;
+        app.emit('stateChange', previousState, value);
+      }
+    }        
+  });
+}, 50); //check button state every 50ms
+*/
